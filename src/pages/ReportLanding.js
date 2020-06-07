a82a66a806dcfc5cbcf108c333f749dd880f15a9
gseeley <50686979+gseeley@users.noreply.github.com>
2020-06-07 15:36:11 +1000

    report landing fixed

--- src/pages/ReportLanding.js
+++ src/pages/ReportLanding.js
@@ -1,55 +1,58 @@
 import React, { useState } from "react";
-import { reportLogin } from "../api";
+import { reportLogin} from "../api";
 import "../css/reportLandingStyles.css";
-import { NavLink, useHistory } from "react-router-dom";
+import Landing from "./Landing";
+import {NavLink, useHistory} from "react-router-dom";
 
 export default function ReportLanding() {
-  const [password, setPassword] = useState("");
+    const [password, setPassword] = useState("");
 
-  let history = useHistory();
+    let history = useHistory();
 
-  async function handleLogin(event) {
-    event.preventDefault();
+    async function handleLogin(event) {
+        event.preventDefault();
 
-    var res;
-    res = await reportLogin({ password });
-    console.log(res);
-    console.log(process.env.ADMIN);
-    if (res != null) {
-      //res is True if the password matches
-      if (res == "True") {
-        history.push("/");
-      } else {
-        alert("Invalid Login");
-      }
+        var res
+        res = (await reportLogin({password}))
+        console.log(res)
+        console.log(process.env.ADMIN);
+        if (res != null){
+            //res is True if the password matches
+            if ( res == "True") {
+                history.push("/admin");
+            }
+            else{
+                alert("Invalid Login")
+            }
+        }
     }
-  }
+    
 
-  function loginButton(event) {
-    event.preventDefault();
-
-    history.push("/admin");
-  }
-
-  return (
+    return (
     <div className="form">
-      <div className="report">
-        <h1 id="title">Admin Access</h1>
-        <form onSubmit={handleLogin}>
-          <label>Password</label>
-          <input
-            type="password"
-            name="password"
-            value={password}
-            onChange={(event) => {
-              setPassword(event.target.value);
-            }}
-          />
-          <button type="submit" onClick={loginButton} className="report-btn">
-            Login
-          </button>
-        </form>
-      </div>
+
+        <div className="Nav-bar">
+        <h2 id="nav-logo-text">Pear</h2>
+        </div>
+        <button type="submit" onClick={handleLogin} className='return-btn'>Return to Home Page</button>
+        
+        <div className="report">
+            <h1 id = "title">
+                Admin Access
+            </h1>
+            <form onSubmit={handleLogin}>
+                <label>Password</label>
+                <input 
+                    type="password" 
+                    name="password"
+                    value={password}
+                    onChange={event => {
+                    setPassword(event.target.value);
+                    }}
+                />
+                <button type="submit" className='adminLogin-btn'>Login</button>
+            </form>
+        </div>
     </div>
-  );
+    );
 }
